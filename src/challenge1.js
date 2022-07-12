857426d59fe48f7d1a0a75a6e9a4723f6f94912b
Chizaram Darius-Mary Ezugwu <100358590+Zara-Ray@users.noreply.github.com>
2022-07-12 10:41:55 +0000

    Challenge1

---
+++ src/challenge1.js
@@ -0,0 +1,12 @@
+function sumOfNumbers(arrayOfNumbers){
+    let total=0;
+    for(let i in arrayOfNumbers) { 
+    }
+    return arrayOfNumbers.length;
+}
+
+var arrayOfNumbers = []; 
+console.log(sumOfNumbers(arrayOfNumbers))
+
+
+
